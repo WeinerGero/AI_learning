5b91d99763d02288516b541e814913495c8eb0d0
WinerGero <korykin.27@mail.ru>
2025-09-21 22:21:52 +0200

    lab_1: created skeleton

---
+++ Корякин_ДМ_lab_1.py
@@ -0,0 +1,29 @@
+import random
+
+
+def simple_probability():
+    return None
+
+def logical_or():
+    return None
+
+def logical_and():
+    return None
+
+def expected_value():
+    return None
+
+def conditional_probability():
+    return None
+
+def bayesian_probability():
+    return None
+
+
+if __name__ == "__main__":
+    print("Simple Probability:", simple_probability())
+    print("Logical OR:", logical_or())
+    print("Logical AND:", logical_and())
+    print("Expected Value:", expected_value())
+    print("Conditional Probability:", conditional_probability())
+    print("Bayesian Probability:", bayesian_probability())
